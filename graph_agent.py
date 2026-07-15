60df9f7445bfce0b03df30c89a1d12a659ca82e0
Shaik Husamuddin <shaikhusamuddin666@gmail.com>
2026-07-15 19:05:39 +0530

    add: implement state graph for managing agent execution flow

--- graph_agent.py
+++ graph_agent.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 from openai import OpenAI
 from typing import TypedDict, Annotated
 from langgraph.graph.message import add_messages
+from langgraph.graph import StateGraph, END
 from agent_core import client, MODEL_NAME, tools, TOOL_MAP
 
 load_dotenv()
@@ -47,3 +48,24 @@ def should_continue(state: AgentState) -> str:
     if last_message.tool_calls:
         return "execute_tools"
     return "END"
+
+
+graph = StateGraph(AgentState)
+
+graph.add_node("call_model", call_model)
+graph.add_node("execute_tools", execute_tools)
+
+graph.set_entry_point("call_model")
+
+graph.add_conditional_edges(
+    "call_model",
+    should_continue,
+    {
+        "execute_tools": "execute_tools",
+        "END": END
+    }
+)
+
+graph.add_edge("execute_tools", "call_model")
+
+app = graph.compile()
